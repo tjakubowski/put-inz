fa8cc8f2cd3141ee005d0c1f61be8cf184e0d6e7
kamilmosek <kamil.mosek@gmail.com>
2021-03-21 18:07:28 +0100

    add appointment api and appointment view

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
+from .models import Appointment, User
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
@@ -14,6 +15,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], validated_data['password'])
-        return user
+
+# Appointment Serializer
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = ('doctorId','userId','datetime','phone','email','note')
+        
+
+
+
+
+        
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, AppointmentSerializer
 from django.shortcuts import redirect
-from .models import User
+from .models import User, Appointment
 import uuid
 
 # Create your views here.
@@ -18,5 +18,20 @@ class RegisterAPI(generics.GenericAPIView):
         user.save()
         
         x = serializer.save()
-        return redirect("/api")
+        return redirect("/")
+
+
+class AppointmentAPI(generics.GenericAPIView):
+        serializer_class = AppointmentSerializer
+
+        def post(self, request, *args, **kwargs):
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True) 
+            appointment = Appointment(doctorId=request.POST['doctorId'],userId=request.POST['userId'],
+            datetime=request.POST['datetime'],phone=request.POST['phone'],email=request.POST['email'],
+            note=request.POST['note'] )
+            appointment.save()
+        
+            x = serializer.save()
+            return redirect("/")
         
