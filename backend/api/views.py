d4e9b19aa552254c902ac95b10d1d20a68d0954b
kamilmosek <kamil.mosek@gmail.com>
2021-03-21 16:28:43 +0100

    add Registration serializers and view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .serializers import UserSerializer, RegisterSerializer
+from django.shortcuts import redirect
 
 # Create your views here.
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return redirect("/api")
+        
