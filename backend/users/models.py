65c48bbc4e45a51838438a7ae71859027998f302
Michał Balcewicz <mxbalcewicz@gmail.com>
2021-03-31 03:53:53 +0200

    INZ-8 Extended User model, added custom UserManager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,28 +1,71 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomizedUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, role, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.role = role
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, role=1, **extra_fields)
+
 
 class Role(models.Model):
     """Roles entries"""
 
-    STAFF = 1
-    PATIENT = 2
+    ADMIN = 1
+    STAFF = 2
+    PATIENT = 3
 
     ROLE_CHOICES = (
+        (ADMIN, 'admin'),
         (STAFF, 'staff'),
-        (PATIENT, 'patient'),
+        (PATIENT, 'patient')
     )
 
-    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, primary_key=True)
 
     def __str__(self):
         return self.get_id_display()
 
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(max_length=30, unique=True)
     role = models.OneToOneField(Role, blank=False, on_delete=models.CASCADE, default=Role.PATIENT)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomizedUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.role}  Mail:{self.email}, Created at:{self.created_at}'
