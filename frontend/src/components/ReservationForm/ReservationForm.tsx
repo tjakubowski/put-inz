79a2551f83f5571c7d4e17b3d1e091d81dc5717f
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2021-04-09 21:17:31 +0200

    INZ-24 Add basic ReservationForm

---
+++ frontend/src/components/ReservationForm/ReservationForm.tsx
@@ -0,0 +1,145 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import { useForm, Controller } from 'react-hook-form';
+
+import { Col, Row } from '../Grid';
+import TextInput from '../Input/TextInput';
+import Button from '../Button';
+import Checkbox from '../Input/Checkbox';
+import Radio from '../Input/Radio';
+
+interface IFormData {
+  firstname: string;
+  lastname: string;
+  email: string;
+  pesel: string;
+  radiotest: string;
+  createAccount: string;
+}
+
+const ReservationForm: React.FC = () => {
+  const theme = useContext(ThemeContext);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFormData>();
+
+  const onSubmit = (data: IFormData) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Row>
+        <Col cols={12} sm={6}>
+          <Controller
+            control={control}
+            name="firstname"
+            rules={{ required: { value: true, message: 'Pole jest wymagane' } }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                label="Imię"
+                block
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.firstname?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col cols={12} sm={6}>
+          <Controller
+            control={control}
+            name="lastname"
+            rules={{ required: { value: true, message: 'Pole jest wymagane' } }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                label="Nazwisko"
+                block
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.lastname?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col cols={12}>
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: { value: true, message: 'Pole jest wymagane' } }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                label="Adres e-mail"
+                type="email"
+                block
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.email?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col cols={12}>
+          <Controller
+            control={control}
+            name="pesel"
+            rules={{ required: { value: true, message: 'Pole jest wymagane' } }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                label="PESEL"
+                block
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                error={errors.pesel?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col cols={12}>
+          <Controller
+            control={control}
+            name="createAccount"
+            rules={{ required: true }}
+            render={({ field: { onChange, value } }) => (
+              <Checkbox
+                name="createAccount"
+                label="Utwórz konto"
+                value={value}
+                onChange={onChange}
+                error={errors.createAccount?.message}
+              />
+            )}
+          />
+        </Col>
+        <Col cols={12}>
+          <Controller
+            control={control}
+            name="radiotest"
+            render={({ field: { onChange, value } }) => (
+              <Radio.Group
+                defaultValue={value}
+                onChange={onChange}
+                error={errors.radiotest?.message}
+              >
+                <Radio label="Test radio 1" value="one" />
+                <Radio label="Test radio 2" value="two" />
+                <Radio label="Test radio 3" value="three" />
+              </Radio.Group>
+            )}
+          />
+        </Col>
+        <Col cols={12}>
+          <Button type="submit" block color={theme.colors.primary}>
+            Send reservation
+          </Button>
+        </Col>
+      </Row>
+    </form>
+  );
+};
+
+export default ReservationForm;
--- frontend/src/views/HomePage.tsx
+++ frontend/src/views/HomePage.tsx
@@ -1,12 +1,13 @@
-import React  from "react";
-import DefaultLayout from "../layouts/DefaultLayout";
-import {Container} from "../components/Grid";
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+import { Container } from '../components/Grid';
+import ReservationForm from '../components/ReservationForm';
 
 const HomePage = () => {
   return (
     <DefaultLayout>
       <Container>
-          Lorem ipsum
+        <ReservationForm />
       </Container>
     </DefaultLayout>
   );
