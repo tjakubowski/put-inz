48c4ca316e3ed1c20eb27e67b851c303f30d16c6
kamilmosek <kamil.mosek@gmail.com>
2021-04-09 20:49:06 +0200

    add patient and staff register view

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,27 +3,29 @@ from django.contrib.auth.hashers import make_password
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 
-class RegisterPatientView(APIView):
-    def post(self, request):
+
+class PatientCreateView(APIView):
+    permission_classes = [AllowAny]
+    def post(self,request):
         user = User()
         user.email = request.data.get('email')
         password = make_password(request.data.get('password'))
         user.set_password(password)
         patient = Patient(user=user, pesel_number=request.data.get('pesel_number'))
-        patient.save()
         user.save()
+        patient.save()
         return Response(status=status.HTTP_201_CREATED)
 
-
-class RegisterStaffView(APIView):
+class StaffCreateView(APIView):
     def post(self, request):
         user = User()
         user.email = request.data.get('email')
         password = make_password(request.data.get('password'))
         user.set_password(password)
         patient = Staff(user=user)
-        patient.save()
         user.save()
+        patient.save()
         return Response(status=status.HTTP_201_CREATED)
