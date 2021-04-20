bba3da265aa686b6b72688a7928a8c3cdbb806f3
kamilmosek <kamil.mosek@gmail.com>
2021-04-20 12:27:33 +0200

    add login path to url.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PatientCreateView, StaffCreateView
+from .views import PatientCreateView, StaffCreateView, PatientLoginView
 
 urlpatterns = [
     path('patient_registration/', PatientCreateView.as_view(), name="PatientCreation"),
     path('staff_registration/', StaffCreateView.as_view(), name="StaffCreation"),
+    path('patient_login/',PatientLoginView.as_view(), name="PatientLogin"),
     
 ]
