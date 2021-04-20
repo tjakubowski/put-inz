f29adcb23a1b2d75497d9e2349651e662d855056
kamilmosek <kamil.mosek@gmail.com>
2021-04-20 12:27:55 +0200

    add permissions and login view

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,12 +4,13 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.contrib.auth.hashers import check_password
 
 
 
 class PatientCreateView(APIView):
     permission_classes = [AllowAny]
-    def post(self,request):
+    def post(self, request):
         user = User()
         user.email = request.data.get('email')
         password = make_password(request.data.get('password'))
@@ -20,6 +21,7 @@ class PatientCreateView(APIView):
         return Response(status=status.HTTP_201_CREATED)
 
 class StaffCreateView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         user = User()
         user.email = request.data.get('email')
@@ -29,3 +31,48 @@ class StaffCreateView(APIView):
         user.save()
         patient.save()
         return Response(status=status.HTTP_201_CREATED)
+
+class PatientLoginView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        try:
+            email = request.data['email']
+            password = request.data['password']
+
+            if User.objects.filter(email=email).exists():
+                user = User.objects.filter(email=email).first()
+                print(user)
+                try:
+                    
+                    valid = user.check_password(password)
+                    #valid = check_password(password, user.password)
+                    #valid = authenticate(email=email,password=password)
+                    #valid = validate_password(password=password,user=user)
+                    
+                    print(valid)
+                    print(password)
+                    print(user)
+                    if valid: 
+                        payload = jwt_payload_handler(user)
+                        token = jwt.encode(payload, settings.SECRET_KEY)
+                        user_details = {}
+                        user_details['email'] = user.email
+                        user_details['token'] = token
+                        user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                        return Response(user_details, status=status.HTTP_200_OK)  
+                    else:
+                        res = {'error': 'wrong password'}
+                        return Response(res, status=status.HTTP_403_FORBIDDEN)
+
+                except Exception as e:
+                    raise e
+            else:
+                res = {
+                    'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+                return Response(res, status=status.HTTP_403_FORBIDDEN)
+        except KeyError:
+            res = {'error': 'please provide a email and a password'}
+            return Response(res)
+
+        
