d0dcf8deb35c1bf0a1b65be46de07222766bcbad
kamilmosek <kamil.mosek@gmail.com>
2021-04-23 13:22:06 +0200

    add registration urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PatientCreateView, StaffCreateView, PatientLoginView, PatientListView, PatientInfoView, StaffInfoView
+from .views import PatientCreateView, ReceptionsitCreateView, LoginView, PatientListView, PatientInfoView, StaffInfoView, ReceptionsitCreateView, DoctorCreateView
 
 urlpatterns = [
     path('patient_registration/', PatientCreateView.as_view(), name="PatientCreation"),
-    path('staff_registration/', StaffCreateView.as_view(), name="StaffCreation"),
-    path('patient_login/',PatientLoginView.as_view(), name="PatientLogin"),
+    path('receptionsit_registration/', ReceptionsitCreateView.as_view(), name="ReceptionsitCreation"),
+    path('doctor_registration/', DoctorCreateView.as_view(), name="DoctorCreation"),
+    path('login/',LoginView.as_view(), name="PatientLogin"),
     path('patient_list/',PatientListView.as_view(),name="PatientList"),
     path('patient_info/',PatientInfoView.as_view(),name="PatientInfoView"),
     path('staff_info/', StaffInfoView.as_view(), name="StaffInfoView")
