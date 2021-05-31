17c443102ad4db1dbd79495c473f3803ed89af41
Michał Balcewicz <mxbalcewicz@gmail.com>
2021-05-31 10:10:14 +0200

    Registered User, Patient, Doctor, Receptionist models for admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Patient, Doctor, Receptionist
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Receptionist)
